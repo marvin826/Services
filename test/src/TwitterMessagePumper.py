cb425ce3da03e21397f83c8b0dd58fe7442323c8
Todd Kaiser <marvin826@comcast.net>
2015-10-04 20:55:03 -0600

    Updates to TwitterMessagePumper.py

--- test/src/TwitterMessagePumper.py
+++ test/src/TwitterMessagePumper.py
@@ -1,15 +1,45 @@
 import json
+import threading
+import Queue
+
+# 
+# declare a Queue to store the messages
+#
+q = Queue.Queue()
 
 #
 # import messages from the json file
 #
 try:
 	msg_file = open("../data/TwitterMessages.json", 'r')
-	messages = json.load(file)
+	msg_obj = json.load(msg_file)
 
-	print str(messages)
+	msg_list = msg_obj["messages"]
+	print str(len(msg_list)) + " messages loaded."
+
+	for msg in msg_list:
+		q.put(msg)
 
 except Exception, e:
 
 	print "Error reading messages from file : " + str(e)
+
+#
+# Setup a timer to push these messages every 
+# five minutes
+#
+def publishMessage():
+	print "Publish message..."
+	msg = q.get()
+	print str(msg)
+	if not q.empty() :
+		t = threading.Timer(60.0, publishMessage)
+		t.start()
+	else:
+		print "message queue empty..."
+
+t = threading.Timer(60.0, publishMessage)
+t.start()
+
+print "Timer started"
 
